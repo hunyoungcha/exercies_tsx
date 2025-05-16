f2b0e830ddea1ac4b17dc602932d3777728b6b2b
hunyoungcha <chamgf5247@naver.com>
2025-05-16 14:50:44 +0900

    first

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <MainBody>
+      <div>hello world</div>
+    </MainBody>
+  );
+}
+
+const MainBody = styled.div`
+  background-color:red;
+`;
